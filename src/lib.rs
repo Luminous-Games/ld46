2754ce549851a9cad38147f33a5a6fd4a76648fb
Ervin Oro <ervinoro@gmail.com>
2020-04-18 23:19:23 +0300

    Add fire

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ macro_rules! log {
 struct SomeWorld {
     renderables: Vec<Rc<RefCell<dyn engine::Renderable>>>,
     player: Rc<RefCell<Player>>,
+    fire: Rc<RefCell<Fire>>,
     last_tick: f64,
 }
 
@@ -46,12 +47,32 @@ impl Renderable for Player {
     }
 }
 
+#[derive(Clone)]
+struct Fire {
+    pos: na::Vector2<f32>,
+}
+
+impl Renderable for Fire {
+    fn render(&self, r: &mut Renderer) {
+        let tm = engine::renderer::TextureMap::new(1, 1);
+        r.draw_quad(
+            na::Vector2::new(self.pos.x, self.pos.y + 32.0),
+            na::Vector2::new(64.0, -64.0),
+            tm.get_texture(0, 0),
+        );
+    }
+}
+
 impl SomeWorld {
     fn new(player: Player) -> SomeWorld {
         let player_in_a_box = Rc::new(RefCell::new(player));
+        let fire_in_a_box = Rc::new(RefCell::new(Fire {
+            pos: na::Vector2::new(300.0, 300.0),
+        }));
         SomeWorld {
-            renderables: vec![player_in_a_box.clone()],
+            renderables: vec![fire_in_a_box.clone(), player_in_a_box.clone()],
             player: player_in_a_box.clone(),
+            fire: fire_in_a_box.clone(),
             last_tick: 0.0,
         }
     }
@@ -83,12 +104,28 @@ impl engine::World for SomeWorld {
     ) -> Vec<Rc<RefCell<dyn Renderable>>> {
         let mut player = self.player.borrow_mut();
         let direction = SomeWorld::get_direction(key_manager);
-        player.speed += direction.scale((timestamp - self.last_tick) as f32 * 0.3);
+        player.speed += direction.scale((timestamp - self.last_tick) as f32 * 0.1);
         let norm = player.speed.norm();
         if norm > 10.0 {
             player.speed.scale_mut(10.0 / norm);
         }
-        player.speed.scale_mut(0.9);
+        player.speed.scale_mut(0.8);
+        let player_pos = &player.pos.clone();
+        let fire_pos = &self.fire.borrow().pos.clone();
+        if (player_pos - fire_pos).norm() < 32.0 {
+            if player_pos.x > fire_pos.x {
+                player.speed.x = f32::max(0.0, player.speed.x);
+            } else {
+                player.speed.x = f32::min(0.0, player.speed.x);
+            }
+            if player_pos.y > fire_pos.y {
+                player.speed.y = f32::max(0.0, player.speed.y);
+            } else {
+                player.speed.y = f32::min(0.0, player.speed.y);
+            }
+            // player.speed = (player_pos - fire_pos).scale(0.1);
+        }
+
         let speed = player.speed.clone();
         player.pos += speed.scale((timestamp - self.last_tick) as f32 * 0.05);
         self.last_tick = timestamp;
